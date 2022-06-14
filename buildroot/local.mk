b5d69198cfcdb7212adbea076efac29a5301283e
mrigendrachaubey <mrigendra.chaubey@gmail.com>
2022-06-14 22:28:48 +0530

    local.mk path for kernel and uboot

--- buildroot/local.mk
+++ buildroot/local.mk
@@ -1,2 +1,2 @@
-LINUX_OVERRIDE_SRCDIR = /home/n/learn/beagleboneblack/bb-kernel/KERNEL
-UBOOT_OVERRIDE_SRCDIR = /home/n/learn/beagleboneblack/u-boot
+LINUX_OVERRIDE_SRCDIR = ../bb-kernel/KERNEL
+UBOOT_OVERRIDE_SRCDIR = ../u-boot
